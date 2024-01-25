51e658fce82a972568e63b15ff4f5ce4af65a6cd
KevinTran99 <kevintrann99@gmail.com>
2024-01-25 12:53:57 +0100

    Generate cards on gallery

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+import { createCard } from './dom.js';
 const menubtn = document.querySelector('#menu-btn');
 const nav = document.querySelector('.nav-links');
+
+const gallery = document.querySelector('#popular-courses');
+
+async function initPage() {
+  const courses = await loadCourses();
+
+  courses.forEach((course) => {
+    gallery.appendChild(createCard(course));
+  });
+}
+
+const loadCourses = async () => {
+  const url = 'http://localhost:3000/courses';
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const result = await response.json();
+    return result;
+  }
+};
 
 menubtn.addEventListener('click', () => {
   nav.classList.toggle('nav-open');
 });
+
+document.addEventListener('DOMContentLoaded', initPage);
